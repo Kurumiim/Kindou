0b09dfa421e34c26346967504e54f923c93f3532
Rukattsu <rukattsu@gmail.com>
2024-05-16 12:43:14 -0300

    Site E-Commerce V1

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'prestashop',
+    'database_password' => 'bgfxrKPqAp1tC3y8NcI7k2hlaGw4jd',
+    'database_prefix' => 'myshop_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'ODa2MxkKsU7AMcTaL83aJ7iXuaPjyDg0powqtpznsuIre990MVbkGSFaTbrPmUV2',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-05-13',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'Xy7qgCUkkKmmoUIv1a7A1yunpdRIpWaifipNbc2K5vdGwjRMLv9Akkdsqk4JiWSP',
+    'cookie_iv' => 'no7uK49dZ8PZQQ3c35PKuq2YEq04Dt3E',
+    'new_cookie_key' => 'def000007b62323d2572de69de71b2fb54549af0078b1da4dc9a5cc89a768fbe56969d7cac339b9e9b4e0adf8aba6e61db184bd5cedc6a1042998d9e807750861aa632d5',
+  ),
+);
